787fc34a0929032749dacd69398f3096868e5d5a
ManishKuPatra254 <manishpatra254@gmail.com>
2023-10-18 22:45:50 +0530

    added images for favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { Userdetails } from './UserDetails/Userdetails';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+            <Userdetails />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/UserDetails/Userdetails.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import styles from './Userdetails.module.css'
+
+export function Userdetails() {
+    return (
+        <Fragment>
+            <div className={styles.main_user_details}>
+                
+            </div>
+        </Fragment>
+    )
+}
