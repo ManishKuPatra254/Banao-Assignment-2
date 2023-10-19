28dbf1cc5ca49390a4d7334fd57509e76330bee5
ManishKuPatra254 <manishpatra254@gmail.com>
2023-10-19 07:41:17 +0530

    added all the css to the user details

--- src/UserDetails/Userdetails.js
+++ src/UserDetails/Userdetails.js
@@ -1,18 +1,26 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import styles from './Userdetails.module.css';
+import { Button } from '@mui/material';
+
 
 export function Userdetails() {
     const [displayName, setDisplayName] = useState([]);
     const [selectedUserId, setSelectedUserId] = useState(null);
 
+
     useEffect(() => {
         fetch('https://602e7c2c4410730017c50b9d.mockapi.io/users')
             .then(data => data.json())
-            .then(response => setDisplayName(response))
+            .then(response => {
+                console.log(response)
+                setDisplayName(response)
+            })
             .catch(error => console.log(error));
     }, []);
 
-    function handleClickNameDisplay(user) {
+
+    function handleClickNameDisplay(id) {
+        const user = displayName.find(user => user.id === id);
         setSelectedUserId(user);
     }
 
@@ -26,7 +34,7 @@ export function Userdetails() {
                             <Fragment>
                                 <div className={styles.names_id_btn}>
                                     <div className={styles.names} key={index}>
-                                        <button onClick={() => handleClickNameDisplay(items.id)}>{items.profile.firstName} {items.profile.lastName}</button>
+                                        <Button sx={{ textTransform: 'unset' }} variant="contained" onClick={() => handleClickNameDisplay(items.id)}>{items.profile.firstName} {items.profile.lastName}</Button>
                                     </div>
                                 </div>
                             </Fragment>
@@ -39,13 +47,23 @@ export function Userdetails() {
                 <div className={styles.sub_user_details2}>
                     <h1>USER DETAILS</h1>
                     {
-                        selectedUserId && (
+                        selectedUserId ? (
                             <Fragment>
-                                <p>{selectedUserId}</p>
-                                <p>{selectedUserId.jobTitle}</p>
-                                <img src={selectedUserId.avatar} alt="" />
+                                <div className={styles.results_d}>
+                                    <h5>USER DETAILS</h5>
+                                    <img src={selectedUserId.avatar} alt="Not found" />
+                                    <p id={styles.pr1}>@{selectedUserId.profile.username}</p>
+                                    <p id={styles.pr2}>{selectedUserId.Bio}</p>
+                                    <h4>Full Name</h4>
+                                    <p id={styles.pr3}>{selectedUserId.profile.firstName} {selectedUserId.profile.lastName}</p>
+                                    <h4>Job Title</h4>
+                                    <p id={styles.pr3}>{selectedUserId.jobTitle}</p>
+                                    <h4>Email</h4>
+                                    <p id={styles.pr3}>{selectedUserId.profile.email}</p>
+                                </div>
                             </Fragment>
-
+                        ) : (
+                            <p>Select a user to see details.</p>
                         )}
                 </div>
             </div>
