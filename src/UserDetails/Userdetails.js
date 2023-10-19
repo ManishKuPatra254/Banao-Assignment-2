534a5b631c931643ae615144e3a0f0aacb4fa695
ManishKuPatra254 <manishpatra254@gmail.com>
2023-10-19 08:31:54 +0530

    added all the css loader

--- src/UserDetails/Userdetails.js
+++ src/UserDetails/Userdetails.js
@@ -1,27 +1,36 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import styles from './Userdetails.module.css';
 import { Button } from '@mui/material';
+import { CircularProgress } from '@mui/material';
 
 
 export function Userdetails() {
     const [displayName, setDisplayName] = useState([]);
-    const [selectedUserId, setSelectedUserId] = useState(null);
+    const [clickedUser, setClickedUser] = useState(null);
+    const [progessLoad, setProgressLoad] = useState(false)
 
 
     useEffect(() => {
+
         fetch('https://602e7c2c4410730017c50b9d.mockapi.io/users')
             .then(data => data.json())
             .then(response => {
                 console.log(response)
                 setDisplayName(response)
+
             })
             .catch(error => console.log(error));
     }, []);
 
 
     function handleClickNameDisplay(id) {
+        setProgressLoad(true)
         const user = displayName.find(user => user.id === id);
-        setSelectedUserId(user);
+        setClickedUser(user);
+        setTimeout(() => {
+            setClickedUser(user);
+            setProgressLoad(false);
+        }, 2000);
     }
 
     return (
@@ -29,7 +38,7 @@ export function Userdetails() {
             <div className={styles.main_user_details}>
                 <div className={styles.sub_user_details1}>
                     <h1>USER LIST</h1>
-                    {displayName.length >= 0 ? (
+                    {
                         displayName.map((items, index) => (
                             <Fragment>
                                 <div className={styles.names_id_btn}>
@@ -39,35 +48,40 @@ export function Userdetails() {
                                 </div>
                             </Fragment>
                         ))
-                    ) : (
-                        <p>Loading user details...</p>
-                    )}
+                    }
                 </div>
 
                 <div className={styles.sub_user_details2}>
                     <h1>USER DETAILS</h1>
-                    {
-                        selectedUserId ? (
+                    {progessLoad ? (
+                        <Fragment>
+                            <div className={styles.image_loader}>
+                                <p><CircularProgress size={100} sx={{ color: 'red' }} /></p>
+                            </div>
+                        </Fragment>
+                    ) : (
+                        clickedUser ? (
                             <Fragment>
                                 <div className={styles.results_d}>
                                     <h5>USER DETAILS</h5>
-                                    <img src={selectedUserId.avatar} alt="Not found" />
-                                    <p id={styles.pr1}>@{selectedUserId.profile.username}</p>
-                                    <p id={styles.pr2}>{selectedUserId.Bio}</p>
+                                    <img src={clickedUser.avatar} alt="Not found" />
+                                    <p id={styles.pr1}>@{clickedUser.profile.username}</p>
+                                    <p id={styles.pr2}>{clickedUser.Bio}</p>
 
                                     <div className={styles.user_info}>
                                         <h4>Full Name</h4>
-                                        <p id={styles.pr3}>{selectedUserId.profile.firstName} {selectedUserId.profile.lastName}</p>
+                                        <p id={styles.pr3}>{clickedUser.profile.firstName} {clickedUser.profile.lastName}</p>
                                         <h4>Job Title</h4>
-                                        <p id={styles.pr3}>{selectedUserId.jobTitle}</p>
+                                        <p id={styles.pr3}>{clickedUser.jobTitle}</p>
                                         <h4>Email</h4>
-                                        <p id={styles.pr3}>{selectedUserId.profile.email}</p>
+                                        <p id={styles.pr3}>{clickedUser.profile.email}</p>
                                     </div>
                                 </div>
                             </Fragment>
                         ) : (
                             <p>Select a user to see details.</p>
-                        )}
+                        )
+                    )}
                 </div>
             </div>
         </Fragment>
